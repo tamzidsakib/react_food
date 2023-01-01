4dcb029ae0303da72c4d20756517f508067107e4
tamzid sakib <tmsakib@gmail.com>
2023-01-01 17:11:56 +0600

    meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,38 +1,48 @@
-import classes from './AvailableMeals.module.css';
+import Card from "../UI/Card";
+import classes from "./AvailableMeals.module.css";
+import MealItem from "./MealItem/MealItem";
 const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
 
-
-const AvailableMeals=()=>{
-    const mealsItem = DUMMY_MEALS.map((meal)=><li>{meal.name}</li>)
-return <section className={classes.meals}>
-    <ul>
-        {mealsItem}
-    </ul>
-</section>
-}
+const AvailableMeals = () => {
+  const mealsItem = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+  return (
+    <section className={classes.meals}>
+        <Card>
+      <ul>{mealsItem}</ul>
+      </Card>
+    </section>
+  );
+};
 export default AvailableMeals;
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import classes from './Card.module.css'
 
-const Card=()=>{
-
+const Card=(props)=>{
+return <div className={classes.card}>
+    {props.children}
+</div>
 }
 
 export default Card;
