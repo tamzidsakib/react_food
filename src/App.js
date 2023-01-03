3e7f6ed762d57c4fe1b48e13171b1fa447c6c551
tamzid sakib <tmsakib@gmail.com>
2023-01-03 16:19:36 +0600

    modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import {Fragment} from 'react'
+import {Fragment, useState} from 'react'
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
+
 function App() {
+  const [showCart, setShowCart]=useState(false)
+
+  const showCartHandler =()=>{
+    setShowCart(true)
+  }
+  const hideCartHandler=()=>{
+    setShowCart(false)
+  }
   return (
     <Fragment>
-      <Header/>
+      {showCart && <Cart onHideCart={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals/>
       </main>
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 const Cart = (props) => {
   const cartItems = (
@@ -8,17 +9,17 @@ const Cart = (props) => {
     </ul>
   );
   return (
-    <div>
+    <Modal onClick={props.onHideCart}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes["button--alt"]} onClick={props.onHideCart}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 export default Cart;
